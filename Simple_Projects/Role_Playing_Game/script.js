43436947d8e6f8afed219c7961e9ed64c88c7860
Zeberio <morandezeb@gmail.com>
2024-01-31 17:42:43 +0300

    Add goStore button property functions

--- Simple_Projects/Role_Playing_Game/script.js
+++ Simple_Projects/Role_Playing_Game/script.js
@@ -22,10 +22,24 @@ const monsterHealthText = document.querySelector('#monsterHealth');
 
 // functions
 
-function goStore() {
+function goTown() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
     button1.onclick = buyHealth;
     button2.onclick = buyWeapon;
     button3.onclick = goTown;
+    text.innerText = "You enter the store.";
+}
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+    text.innerText = "You enter the store.";
 }
 
 function goCave() {
@@ -35,6 +49,11 @@ function goCave() {
 function fightDragon() {
     console.log('Fighting dragon.');
 }
+
+function buyHealth() {}
+
+function buyWeapon() {}
+
 
 // initialize buttons
 
