289e7e2a5c1336639323dbbe1c419380bd7e88c5
Zeberio <morandezeb@gmail.com>
2024-01-31 19:45:14 +0300

    Buy weapon

--- Simple_Projects/Role_Playing_Game/script.js
+++ Simple_Projects/Role_Playing_Game/script.js
@@ -87,7 +87,12 @@ function buyHealth() {
     }
 }
 
-function buyWeapon() {}
+function buyWeapon() {
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeapon += 1;
+    }
+}
 
 function fightSlime() {}
 
