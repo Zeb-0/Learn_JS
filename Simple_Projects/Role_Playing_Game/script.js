ddc5e272a991fb516bd9d14488522eff9ea51436
Zeberio <morandezeb@gmail.com>
2024-01-31 19:53:38 +0300

    Display upgrades

--- Simple_Projects/Role_Playing_Game/script.js
+++ Simple_Projects/Role_Playing_Game/script.js
@@ -91,6 +91,8 @@ function buyWeapon() {
     if (gold >= 30) {
         gold -= 30;
         currentWeapon += 1;
+        goldText.innerText = gold;
+        text.innerText = "You now have a new weapon."
     }
 }
 
