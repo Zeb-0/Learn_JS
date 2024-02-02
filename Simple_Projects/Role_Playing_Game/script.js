4ebd474f6d6a2ba97d09bcd1e7878fa8985c3a1c
Zeberio <morandezeb@gmail.com>
2024-02-02 19:51:45 +0300

    Make the game more dynamic

--- Simple_Projects/Role_Playing_Game/script.js
+++ Simple_Projects/Role_Playing_Game/script.js
@@ -182,7 +182,7 @@ function goFight() {
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks";
     text.innerText += "You attack it with your " + weapons[currentWeapon].name + ".";
-    health = monsters[fighting].level;
+    health = getMonsterAttackValue(monsters[fighting].level);
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
@@ -197,6 +197,9 @@ function attack() {
         }
     }
 }
+
+function getMonsterAttackValue(level) {}
+
 function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
 }
