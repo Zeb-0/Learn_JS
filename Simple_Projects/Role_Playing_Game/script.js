14ee5683a5a9260585f113d0237f67e7d67ea249
Zeberio <morandezeb@gmail.com>
2024-02-03 21:26:34 +0300

    some adjustments

--- Simple_Projects/Role_Playing_Game/script.js
+++ Simple_Projects/Role_Playing_Game/script.js
@@ -188,7 +188,7 @@ function goFight() {
 
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks";
-    text.innerText += "You attack it with your " + weapons[currentWeapon].name + ".";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health = getMonsterAttackValue(monsters[fighting].level);
 
     if (isMonsterHit()) {
