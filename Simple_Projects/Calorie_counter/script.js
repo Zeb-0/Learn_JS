1059ce8bc8001ff66350bab2d936daee17756bfb
Zeberio <morandezeb@gmail.com>
2024-02-06 22:04:51 +0300

    Now the functions

--- Simple_Projects/Calorie_counter/script.js
+++ Simple_Projects/Calorie_counter/script.js
@@ -6,3 +6,8 @@ const entryDropdown = document.getElementById("entry-dropdown");
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
+
+let isError = false;
+
+// Functions:
+function cleanInputString(str) {}
