817a5e61cbd88997dd41482570f5d061f6100636
Zeberio <morandezeb@gmail.com>
2024-02-06 21:57:07 +0300

    more variables

--- Simple_Projects/Calorie_counter/script.js
+++ Simple_Projects/Calorie_counter/script.js
@@ -1,5 +1,5 @@
 // Code for Calorie Counter
 
-//Variables
-
 const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
