8ddfc616f1889d146db303b75f215ed1ec341b47
Zeberio <morandezeb@gmail.com>
2024-02-07 19:31:25 +0300

    check the list values generated

--- Simple_Projects/Calorie_counter/script.js
+++ Simple_Projects/Calorie_counter/script.js
@@ -35,4 +35,7 @@ addEntryButton.addEventListener("click", addEntry);
 
 function getCaloriesFromInputs(list) {
     let calories = 0;
+    for (let i = 0; i < list.length; i++) {
+        const currVal = list[i].value;
+    }
 }
