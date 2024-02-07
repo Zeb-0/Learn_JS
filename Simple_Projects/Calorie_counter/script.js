9129220d7714bfc17bacc8192efabd8bff5ebd4c
Zeberio <morandezeb@gmail.com>
2024-02-07 19:35:52 +0300

    clean up

--- Simple_Projects/Calorie_counter/script.js
+++ Simple_Projects/Calorie_counter/script.js
@@ -36,6 +36,6 @@ addEntryButton.addEventListener("click", addEntry);
 function getCaloriesFromInputs(list) {
     let calories = 0;
     for (let i = 0; i < list.length; i++) {
-        const currVal = list[i].value;
+        const currVal = cleanInputString(list[i].value);
     }
 }
