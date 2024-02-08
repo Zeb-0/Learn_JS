e7e27b52eb71ebc9cc6deb7bd0921d6f0a4d1413
Zeberio <morandezeb@gmail.com>
2024-02-08 14:31:57 +0300

    Add calories Consumed and Burned

--- Simple_Projects/Calorie_counter/script.js
+++ Simple_Projects/Calorie_counter/script.js
@@ -78,5 +78,7 @@ function calculateCalories(e) {
     <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
     <hr>
     <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
     `;
 }
