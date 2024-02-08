b16ce33688d38b3f3c30e3696938064b2897a836
Zeberio <morandezeb@gmail.com>
2024-02-08 14:29:25 +0300

    Display the budgeted calories

--- Simple_Projects/Calorie_counter/script.js
+++ Simple_Projects/Calorie_counter/script.js
@@ -77,7 +77,6 @@ function calculateCalories(e) {
     output.innerHTML = `
     <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
     <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
     `;
 }
-
-// ${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}
