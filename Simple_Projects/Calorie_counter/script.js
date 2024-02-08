93e2807b45ebabd089e4d25a6c8a46b74f7b11ca
Zeberio <morandezeb@gmail.com>
2024-02-08 13:54:38 +0300

    check for invalid input

--- Simple_Projects/Calorie_counter/script.js
+++ Simple_Projects/Calorie_counter/script.js
@@ -66,4 +66,7 @@ function calculateCalories(e) {
     const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
 
     const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+    if (isError) {
+        return;
+    }
 }
