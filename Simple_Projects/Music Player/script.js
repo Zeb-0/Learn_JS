7709fbd925577351be8048fcb52cbcdd04affafc
Zeberio <morandezeb@gmail.com>
2024-02-15 23:26:06 +0300

    sort alphabetically

--- Simple_Projects/Music Player/script.js
+++ Simple_Projects/Music Player/script.js
@@ -60,5 +60,12 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songsHTML;
 };
 
-userData?.songs.sort((a, b) => {});
+userData?.songs.sort((a, b) => {
+    if (a.title < b.title) {
+        return -1;
+    }
+    if (a.title > b.title) {
+        return 1;
+    }
+});
 renderSongs(userData?.songs);
