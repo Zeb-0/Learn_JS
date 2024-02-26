9ea22ac97a6e8a2656f524ce59cbe47655ed675e
Zeberio <morandezeb@gmail.com>
2024-02-26 17:44:03 +0300

    finally pause the song

--- Simple_Projects/Music Player/script.js
+++ Simple_Projects/Music Player/script.js
@@ -108,6 +108,9 @@ const playSong = (id) => {
 
 const pauseSong = () => {
     userData.songCurrentTime = audio.currentTime;
+    playButton.classList.remove('playing');
+
+    audio.pause();
 };
 
 const renderSongs = (array) => {
