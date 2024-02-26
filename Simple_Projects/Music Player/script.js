56c72a83f5b875b5c322fcda0f5bc473cdf49863
Zeberio <morandezeb@gmail.com>
2024-02-26 17:29:56 +0300

    continue playing the current song

--- Simple_Projects/Music Player/script.js
+++ Simple_Projects/Music Player/script.js
@@ -131,6 +131,8 @@ const renderSongs = (array) => {
 playButton.addEventListener('click', () => {
     if (userData?.currentSong === null){
         playSong(userData?.songs[0].id);
+    } else {
+        playSong(userData?.currentSong.id);
     }
 });
 
