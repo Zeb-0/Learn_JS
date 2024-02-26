da3b05f57c0b8b5f9ade95612ca7c328d68aecef
Zeberio <morandezeb@gmail.com>
2024-02-26 16:42:26 +0300

    .....

--- Simple_Projects/Music Player/script.js
+++ Simple_Projects/Music Player/script.js
@@ -75,4 +75,8 @@ const playSong = (id) => {
     const song = userData?.songs.find((song) => song.id ===id);
     audio.srs = song.src;
     audio.title = song.title;
+
+    if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+        audio.currentTime = 0;
+      }
 };
