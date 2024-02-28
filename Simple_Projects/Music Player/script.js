d77acf2f8316ab2bd8373051a532e05961db90ac
Zeberio <morandezeb@gmail.com>
2024-02-28 13:49:15 +0300

    add the attribute back

--- Simple_Projects/Music Player/script.js
+++ Simple_Projects/Music Player/script.js
@@ -142,6 +142,10 @@ const highlightCurrentSong = () => {
   playlistSongElements.forEach((songEl) => {
     songEl.removeAttribute("aria-current");
   });
+
+  if (songToHighlight) {
+    songToHighlight.setAttribute('aria-current', 'true');
+  }
 };
 
 // Event Listeners:
