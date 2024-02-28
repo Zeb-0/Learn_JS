a2f952a2440f40a44879811469e83034cd94161a
Zeberio <morandezeb@gmail.com>
2024-02-28 12:32:34 +0300

    get soongs index

--- Simple_Projects/Music Player/script.js
+++ Simple_Projects/Music Player/script.js
@@ -137,6 +137,8 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songsHTML;
 };
 
+const getCurrentSongIndex = () => {};
+
 playButton.addEventListener('click', () => {
     if (userData?.currentSong === null){
         playSong(userData?.songs[0].id);
